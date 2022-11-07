796515c64d4326661e274ae5e4983a9556a0ec7f
Elaine Pereira <112098075+elainepereiraf@users.noreply.github.com>
2022-11-07 15:07:58 -0300

    Create Jokempo.py

---
+++ Jokempo.py
@@ -0,0 +1,26 @@
+#Crie uma função que simule o jogo do jokempô, isto é, dada a entrada de dois jogadores, retorne a indicação de qual deles venceu.
+
+def Jokempo(JogadorA, JogadorB):
+    if JogadorA == 'Pedra':
+        if JogadorB == 'Tesoura':
+            print('Jogador A ganhou!')
+        elif JogadorB == 'Papel':
+            print('Jogador B ganhou!')
+        else:
+            print('Empate!')
+    
+    if JogadorA == 'Tesoura':
+        if JogadorB == 'Papel':
+            print('Jogador A ganhou!')
+        elif JogadorB == 'Pedra':
+            print('Jogador B ganhou!')
+        else:
+            print('Empate!')
+    
+    if JogadorA == 'Papel':
+        if JogadorB == 'Pedra':
+            print('Jogador A ganhou!')
+        elif JogadorB == 'Tesoura':
+            print('Jogador B ganhou!')
+        else:
+            print('Empate!')
